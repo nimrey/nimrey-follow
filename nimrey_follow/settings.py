863547f0d491c888bcaa9edbbd5dda8a60051207
Mohammed Salman <msalmail444@gmail.com>
2018-08-28 13:24:17 +0300

    as

--- nimrey_follow/settings.py
+++ nimrey_follow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'p@_)^a(rpds+x9%u!mf!x98335sijwb%b4gs&f20srhm$a*roo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.pythonanywhere.com']
 
 
 # Application definition
@@ -119,10 +119,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = '/home/nimrey/nimrey-follow/static'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'msal'
